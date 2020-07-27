22f5c727987f8446bd52c52cd2222e5205928f72
Didik Purwanto <me@didpurwanto.com>
2020-07-27 21:44:00 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -20,31 +20,31 @@ export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 Now, let's work on it. Follow this instruction. If you found any trouble, let me know in comments.
 
+sudo apt-get update
 sudo apt-get install git cmake
 sudo apt-get install libatlas-base-dev gfortran
 sudo apt-get install libhdf5-serial-dev hdf5-tools
 sudo apt-get install python3-dev
 sudo apt-get update
-sudo pip3 install -U pip testresources setuptools
 
 Install Pip
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 rm get-pip.py
-
-These installs took ~35 minutes
+sudo pip3 install -U pip testresources setuptools
 
 pip3 install numpy==1.16.1
+
+sudo pip3 install future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo apt-get install pkg-config libhdf5-100 libhdf5-dev
 pip3 install scipy
 pip3 install keras==2.3.0
 
-future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
-sudo apt-get install pkg-config libhdf5-100 libhdf5-dev
 Download tensorflow here
 https://developer.download.nvidia.com/compute/redist/jp/v43/tensorflow/
 download the .whl file and install it
 
-It will took some times.
+It will take some times.
 
 If already finished, now, we can try the demo.
 git clone https://github.com/dusty-nv/jetson-inference
