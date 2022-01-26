8486246af1be058bb6619004740743a0381a9403
Didik P <me@didpurwanto.com>
2022-01-26 12:20:17 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -47,17 +47,4 @@ python3 setup.py install --user
 cd ../
 sudo pip3 install 'pillow<7'
 
-# verification
-import torch
->>> print(torch.__version__)
->>> print('CUDA available: ' + str(torch.cuda.is_available()))
->>> print('cuDNN version: ' + str(torch.backends.cudnn.version()))
->>> a = torch.cuda.FloatTensor(2).zero_()
->>> print('Tensor a = ' + str(a))
->>> b = torch.randn(2).cuda()
->>> print('Tensor b = ' + str(b))
->>> c = a + b
->>> print('Tensor c = ' + str(c))
 
-import torchvision
->>> print(torchvision.__version__)
